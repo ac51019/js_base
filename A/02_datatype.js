cb6344ac8f0b943648fc3460f48175f8eb18c321
Anshu Chauhan <101622044+ac51019@users.noreply.github.com>
2024-02-23 17:34:54 +0000

    dataatypes

---
+++ A/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+//alert("Hello Moto"); // we are using node js, not browser
+
+console.log(3 + 3); 
+// code readability should be high
+console.log("Dhairya")
+
+
+let name = "Dhairya" // string
+let age = 3 //number
+let isLoggedIn  =  true  // boolean
+
+
+//   DATATYPE
+
+// number - 2 to power 53
+// bigint
+//string - "", ''
+//boolean - true/false
+//null - standlone valuse
+// undefined -
+// symbol - unique
+
+
+// Objects
+
+console.log(typeof "Dhairya");
+
+console.log(typeof undefined); // this is undefined
+console.log(typeof null); // object
