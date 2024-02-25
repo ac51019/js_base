60c066299084291da79a90bd03c056b9b9095398
Anshu Chauhan <101622044+ac51019@users.noreply.github.com>
2024-02-25 06:52:10 +0000

    Add operater

--- A/03_conversion.js
+++ A/03_conversion.js
@@ -7,14 +7,14 @@ let score = null
 
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log (valueInNumber)
+// console.log (valueInNumber)
 
 
 
@@ -27,7 +27,7 @@ let isLoggedIn = "dhairya"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // 1- ture, 0-false
@@ -37,6 +37,46 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 let stringNumber =String(someNumber)
 
-console.log(stringNumber)
+// console.log(stringNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+//----------- Operations -------
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello "
+let str2 = "Ram"
+
+let str3 = str1 +str2
+
+// console.log(str3);
+
+// console.log("1" +3 );
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true+);
+// console.log();
+
+let num1, num2, num3
+
+let gameCounter = 100
+
+gameCounter++;
+
+console.log(gameCounter);
