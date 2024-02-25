9be482c60b96c7ef593bd2ff95656c9c4db2f3c7
Anshu Chauhan <101622044+ac51019@users.noreply.github.com>
2024-02-25 07:30:12 +0000

    update and new

---
+++ A/04_comparison_of_datatype.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("02" > 1);
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// The resone is that an equality check == and comparisons > < >= <= work differently.
+// comparisons convert null to a number, treating is as 0. that's why (3) null >=0 is true and >0 is false.
+
+
+console.log(undefined == 0);
+
+
+
+//===
+
+console.log("2" === 2);
+
---
+++ A/05_dt_summery.js
@@ -0,0 +1,34 @@
+//  primitive datatype
+
+// 7 type - string,Number, boolearn, Null, Undefined, Symbol,bigint
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+// const id = symbol('123456')
+const anotherId = symbol('123456')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3249283742934727237n
+
+// refeence (Non-Primitive)
+
+// Array, Objects, functions
+
+const heros= ["shaktiman", "naagraj","doga"];
+let myObj={
+    name = "DHAIRYA",
+    age = 03,
+
+}
+
+function myFunction = function(){
+    console.log("Hello World") 
+    
+}
+
+console.log(typeof bigNumber );
